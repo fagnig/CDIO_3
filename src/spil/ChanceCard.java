578e5f037bda12eed4f2d4a0f2d67f2c0a9a5ccc
Matthias1997 <32457240+Matthias1997@users.noreply.github.com>
2017-11-21 14:23:08 +0100

    added break;

--- src/spil/ChanceCard.java
+++ src/spil/ChanceCard.java
@@ -17,60 +17,79 @@ public class ChanceCard {
 		case 0 :{
 			
 		}
+	break;
 		case 1 :{
 			
 		}
+	break;
 		case 2 :{ 
 			
 		}
+	break;
 		case 3 :{
 			
 		}
+	break;
 		case 4 :{
 			
 		}
+	break;
 		case 5 :{
 			
 		}
+	break;
 		case 6 :{
 			player.account.add(-2);
 		}
+	break;
 		case 7 :{
 			
 		}
+	break;
 		case 8 :{
 			
 		}
+	break;
 		case 9 :{
 			
 		}
+	break;
 		case 10 :{
 			
 		}
+	break;
 		case 11 :{
 			
 		}
+	break;
 		case 12 :{
 			
 		}
+	break;
 		case 13 :{
 			
 		}
+	break;
 		case 14 :{
 			
 		}
+	break;
 		case 15 :{
 			
 		}
+	break;
 		case 16 :{
 			
 		}
+	break;
 		case 17 :{
 			
 		}
+	break;
 		case 18 :{
 			
 		}
+	break;
 		case 19 :{  
 			
 		}
